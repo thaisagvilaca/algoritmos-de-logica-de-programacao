9af96d7a6b0466b2c6007930084aa2924c88686c
thaisagvilaca <thaisa_vilaca@hotmail.com>
2025-05-06 16:13:18 -0300

    Algoritmos para calculo de perimetro, area e diametro

---
+++ perimetro-area-diametro.py
@@ -0,0 +1,18 @@
+pi = 3.14
+
+raio = float(input("Digite a medida do raio do círculo (em cm): "))
+
+perimetro = float(2*pi)*raio
+
+area = float(pi*(raio**2))
+
+diametro = float(2*raio)
+
+print(f"O perímetro do círculo é: {perimetro:.2f} cm")
+
+print(f"A área do círculo é: {area:.2f} cm")
+
+print(f"O diâmetro do círculo é: {diametro:.2f} cm")
+
+
+
