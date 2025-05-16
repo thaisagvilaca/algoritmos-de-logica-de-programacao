7a02f08b7d05abd3103e268918b201adef944aac
thaisagvilaca <thaisa_vilaca@hotmail.com>
2025-05-16 16:14:44 -0300

    Algoritmo de Lista para adicionar, alterar e remover elementos

---
+++ lista1.py
@@ -0,0 +1,10 @@
+frutas = ['morango', 'abacate', 'goiaba', 'acerola', 'banana', 'jaca', 'uva', 'melão']
+
+frutas.append('manga')
+frutas.pop(2)
+frutas.pop()
+frutas[0] = 'laranja'
+print(frutas[1])
+
+
+
