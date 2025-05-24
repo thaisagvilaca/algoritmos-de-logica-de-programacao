92d17b64f3a56cf74143cd2f97ba855a2f321ed6
thaisagvilaca <thaisagvilaca@gmail.com>
2025-05-24 09:59:00 -0300

    Algoritmo para cálculo, usando módulos e pacotes

---
+++ modulos_e_pacotes.py
@@ -0,0 +1,28 @@
+import statistics
+lista = [20, 43, 55, 61, 89, 90, 103, 115, 138, 209, 265, 387, 405, 471]
+media = statistics.mean(lista)
+mediana = statistics.median(lista)
+print(media)
+print(mediana)
+
+import statistics as st
+lista = [20, 43, 55, 61, 89, 90, 103, 115, 138, 209, 265, 387, 405, 471]
+media = st.mean(lista)
+mediana = st.median(lista)
+print(media)
+print(mediana)
+
+from statistics import mean, median
+lista = [20, 43, 55, 61, 89, 90, 103, 115, 138, 209, 265, 387, 405, 471]
+media = mean(lista)
+mediana = median(lista)
+print(media)
+print(mediana)
+
+from statistics import *
+lista = [20, 43, 55, 61, 89, 90, 103, 115, 138, 209, 265, 387, 405, 471]
+media = mean(lista)
+mediana = median(lista)
+print(media)
+print(mediana)
+
